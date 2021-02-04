5919f0ba31d26fef9ed6806653b0652c400a4fb8
machinesandpixels <2016quezadaj@gmail.com>
2021-02-04 15:58:04 +0000

    Answered question 4 of low-level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char string[52];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+	puts(string);
+	return 0;
+}
